fd10eca4dfa8db933b581cd72ebcfe0007615bc1
gabii6431 <gabii6431@gmail.com>
2019-06-18 09:22:12 -0300

    Complete_Link e Single_link convertidos em métodos

--- complete_link.py
+++ complete_link.py
@@ -20,102 +20,106 @@ def le_arq():
     return dados
 
 
-dados = le_arq()
-numObservacao = dados.shape[0]
-index = {}
-grupo = []
-for p in range(numObservacao):
-    index.update({p: [p]})
-    grupo.append(p)
-tamanhoMatriz = len(index)
-quantidadeGrupo = numObservacao - 1
-matriz_dendograma = np.zeros(((numObservacao-1), 4))
-menor = 1000
-mini = 1000
-pos_min = [0, 0]
-for z in range(tamanhoMatriz-1):
-    matriz_similaridade = np.zeros((tamanhoMatriz, tamanhoMatriz))
-    for i in range(len(list(index))):
-        for j in range(len(list(index))):
-            if j >= i:
-                continue
-            else:
-                if len(index[list(index)[i]]) == 1 and len(index[list(index)[j]]) == 1:
-                    matriz_similaridade[i][j] = round(
-                        dist_euclidiana(dados[index[list(index)[i]][0]], dados[index[list(index)[j]][0]]), 3)
-                elif len(index[list(index)[i]]) == 1 and len(index[list(index)[j]]) != 1:
-                    maior = -1000
-                    for q in range(len(index[list(index)[j]])):
-                        dist = round(dist_euclidiana(
-                            dados[index[list(index)[i]][0]], dados[index[list(index)[j]][q]]), 3)
-                        if maior < dist:
-                            maior = dist
-                    matriz_similaridade[i][j] = maior
-                elif len(index[list(index)[i]]) != 1 and len(index[list(index)[j]]) == 1:
-                    maior = -1000
-                    for q in range(len(index[list(index)[i]])):
-                        dist = round(dist_euclidiana(
-                            dados[index[list(index)[i]][q]], dados[index[list(index)[j]][0]]), 3)
-                        if maior < dist:
-                            maior = dist
-                    matriz_similaridade[i][j] = maior
+def complete_link():
+    dados = le_arq()
+    numObservacao = dados.shape[0]
+    index = {}
+    grupo = []
+    for p in range(numObservacao):
+        index.update({p: [p]})
+        grupo.append(p)
+    tamanhoMatriz = len(index)
+    quantidadeGrupo = numObservacao - 1
+    matriz_dendograma = np.zeros(((numObservacao-1), 4))
+    maior = -1000
+    mini = 1000
+    pos_min = [0, 0]
+    for z in range(tamanhoMatriz-1):
+        matriz_similaridade = np.zeros((tamanhoMatriz, tamanhoMatriz))
+        for i in range(len(list(index))):
+            for j in range(len(list(index))):
+                if j >= i:
+                    continue
                 else:
-                    maior = -1000
-                    for x in range(len(index[list(index)[i]])):
-                        for y in range(len(index[list(index)[j]])):
+                    if len(index[list(index)[i]]) == 1 and len(index[list(index)[j]]) == 1:
+                        matriz_similaridade[i][j] = round(
+                            dist_euclidiana(dados[index[list(index)[i]][0]], dados[index[list(index)[j]][0]]), 3)
+                    elif len(index[list(index)[i]]) == 1 and len(index[list(index)[j]]) != 1:
+                        maior = -1000
+                        for q in range(len(index[list(index)[j]])):
                             dist = round(dist_euclidiana(
-                                dados[index[list(index)[i]][x]], dados[index[list(index)[j]][y]]), 3)
+                                dados[index[list(index)[i]][0]], dados[index[list(index)[j]][q]]), 3)
                             if maior < dist:
                                 maior = dist
-                    matriz_similaridade[i][j] = maior
-    print(matriz_similaridade)
-    for i in range(tamanhoMatriz):
-        for j in range(tamanhoMatriz):
-            if j >= i:
-                continue
+                        matriz_similaridade[i][j] = maior
+                    elif len(index[list(index)[i]]) != 1 and len(index[list(index)[j]]) == 1:
+                        maior = -1000
+                        for q in range(len(index[list(index)[i]])):
+                            dist = round(dist_euclidiana(
+                                dados[index[list(index)[i]][q]], dados[index[list(index)[j]][0]]), 3)
+                            if maior < dist:
+                                maior = dist
+                        matriz_similaridade[i][j] = maior
+                    else:
+                        maior = -1000
+                        for x in range(len(index[list(index)[i]])):
+                            for y in range(len(index[list(index)[j]])):
+                                dist = round(dist_euclidiana(
+                                    dados[index[list(index)[i]][x]], dados[index[list(index)[j]][y]]), 3)
+                                if maior < dist:
+                                    maior = dist
+                        matriz_similaridade[i][j] = maior
+        print(matriz_similaridade)
+        for i in range(tamanhoMatriz):
+            for j in range(tamanhoMatriz):
+                if j >= i:
+                    continue
+                else:
+                    if matriz_similaridade[i][j] <= mini:
+                        mini = matriz_similaridade[i][j]
+                        pos_min = [i, j]
+        print(pos_min)
+        if len(index[list(index)[pos_min[0]]]) == 1:
+            quantidadeGrupo = quantidadeGrupo + 1
+            matriz_dendograma[z][0] = grupo[list(index)[pos_min[0]]]
+            matriz_dendograma[z][1] = grupo[list(index)[pos_min[1]]]
+            if len(index[list(index)[pos_min[1]]]) == 1:
+                grupo[list(index)[pos_min[0]]] = quantidadeGrupo
+                grupo[list(index)[pos_min[1]]] = quantidadeGrupo
             else:
-                if matriz_similaridade[i][j] <= mini:
-                    mini = matriz_similaridade[i][j]
-                    pos_min = [i, j]
-    print(pos_min)
-    if len(index[list(index)[pos_min[0]]]) == 1:
-        quantidadeGrupo = quantidadeGrupo + 1
-        matriz_dendograma[z][0] = grupo[list(index)[pos_min[0]]]
-        matriz_dendograma[z][1] = grupo[list(index)[pos_min[1]]]
-        if len(index[list(index)[pos_min[1]]]) == 1:
-            grupo[list(index)[pos_min[0]]] = quantidadeGrupo
-            grupo[list(index)[pos_min[1]]] = quantidadeGrupo
+                grupo[list(index)[pos_min[0]]] = quantidadeGrupo
+                for u in range(len(index[list(index)[pos_min[1]]])):
+                    grupo[index[list(index)[pos_min[1]]][u]] = quantidadeGrupo
+            index[list(index)[pos_min[1]]].append(list(index)[pos_min[0]])
+            matriz_dendograma[z][2] = mini
+            matriz_dendograma[z][3] = len(index[list(index)[pos_min[1]]])
         else:
-            grupo[list(index)[pos_min[0]]] = quantidadeGrupo
-            for u in range(len(index[list(index)[pos_min[1]]])):
-                grupo[index[list(index)[pos_min[1]]][u]] = quantidadeGrupo
-        index[list(index)[pos_min[1]]].append(list(index)[pos_min[0]])
-        matriz_dendograma[z][2] = mini
-        matriz_dendograma[z][3] = len(index[list(index)[pos_min[1]]])
-    else:
-        quantidadeGrupo = quantidadeGrupo + 1
-        matriz_dendograma[z][0] = grupo[list(index)[pos_min[0]]]
-        matriz_dendograma[z][1] = grupo[list(index)[pos_min[1]]]
-        if len(index[list(index)[pos_min[1]]]) == 1:
-            grupo[list(index)[pos_min[1]]] = quantidadeGrupo
-            for u in range(len(index[list(index)[pos_min[0]]])):
-                grupo[index[list(index)[pos_min[0]]][u]] = quantidadeGrupo
-        else:
-            for u in range(len(index[list(index)[pos_min[0]]])):
-                grupo[index[list(index)[pos_min[0]]][u]] = quantidadeGrupo
-            for v in range(len(index[list(index)[pos_min[1]]])):
-                grupo[index[list(index)[pos_min[1]]][v]] = quantidadeGrupo       
-        for s in range(len(index[list(index)[pos_min[0]]])):
-            index[list(index)[pos_min[1]]].append(
-                index[list(index)[pos_min[0]]][s])
-        matriz_dendograma[z][2] = mini
-        matriz_dendograma[z][3] = len(index[list(index)[pos_min[1]]])
-    index.pop(list(index)[pos_min[0]])
-    tamanhoMatriz = len(index)
-    print(index)
-    mini = 1000
-    print(grupo)
-print(matriz_dendograma)
-dendrogram = dendrogram(matriz_dendograma, truncate_mode='none')
-plt.title("Agrupamento Hierarquico")
-plt.show()
+            quantidadeGrupo = quantidadeGrupo + 1
+            matriz_dendograma[z][0] = grupo[list(index)[pos_min[0]]]
+            matriz_dendograma[z][1] = grupo[list(index)[pos_min[1]]]
+            if len(index[list(index)[pos_min[1]]]) == 1:
+                grupo[list(index)[pos_min[1]]] = quantidadeGrupo
+                for u in range(len(index[list(index)[pos_min[0]]])):
+                    grupo[index[list(index)[pos_min[0]]][u]] = quantidadeGrupo
+            else:
+                for u in range(len(index[list(index)[pos_min[0]]])):
+                    grupo[index[list(index)[pos_min[0]]][u]] = quantidadeGrupo
+                for v in range(len(index[list(index)[pos_min[1]]])):
+                    grupo[index[list(index)[pos_min[1]]][v]] = quantidadeGrupo       
+            for s in range(len(index[list(index)[pos_min[0]]])):
+                index[list(index)[pos_min[1]]].append(
+                    index[list(index)[pos_min[0]]][s])
+            matriz_dendograma[z][2] = mini
+            matriz_dendograma[z][3] = len(index[list(index)[pos_min[1]]])
+        index.pop(list(index)[pos_min[0]])
+        tamanhoMatriz = len(index)
+        print(index)
+        mini = 1000
+        print(grupo)
+    print(matriz_dendograma)
+    dendrogram(matriz_dendograma, truncate_mode='none')
+    plt.title("Agrupamento Hierarquico")
+    plt.show()
+
+
+complete_link()
